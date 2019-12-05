64183ad7d370e10353473c79763d2e3e29471856
Astyak Ghanavatian <astyakghanavatian@gmail.com>
2019-12-05 14:07:01 -0500

    Updated stuff, USE SEQUENCE

--- src/xray.py
+++ src/xray.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing.image import ImageDataGenerator
 import glob
 import csv
 import re
@@ -47,8 +48,11 @@ print(pd.DataFrame(cleaned_data))
 
 
 # --- fill binary matrix with images and their labels
+# --- keep track of indices prior to train/test split
 labels_matrix = np.zeros((12120,15), dtype=int)
+img_index = {}
 for i in cleaned_data[:]:
+    img_index[cleaned_data.index(i)] = i[0]
     temp = i[1].split("|")
     #print("AFTER SPLIT: ", temp)
     for t in temp[:]:
@@ -58,24 +62,46 @@ for i in cleaned_data[:]:
 
 print(pd.DataFrame(labels_matrix))
 
+print(img_index[0])
 
-"""
+
 X_train, X_test = train_test_split(cleaned_data, test_size=0.20)
 
-print("TRAIN SPLIT: \n")
-print(pd.DataFrame(X_train))
 
-print("TEST SPLIT: \n")
-print(pd.DataFrame(X_test))
+train_data = pd.DataFrame(X_train)
+test_data = pd.DataFrame(X_test)
+
+
+# --- KERAS SEQUENCE FOR BATCH PROCESSING???????????
+"""
+datagen = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True)
 """
 
 
+#print("TRAIN SPLIT: \n")
+#print(pd.DataFrame(X_train))
+
+#print("TEST SPLIT: \n")
+#print(pd.DataFrame(X_test))
+
+
+"""
 kf = KFold(n_splits=5)
 
 indices = kf.split(cleaned_data)
+print(type(indices))
+
 
 # --- retrieve train and test splits to run in Neural Network
 for train_index, test_index in indices:
     print("TRAIN:", train_index, "TEST:", test_index)
     #X_train, X_test = cleaned_data[train_index], cleaned_data[test_index]
 
+train_data = pd.DataFrame()
+"""
